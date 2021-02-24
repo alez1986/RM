b44593e52ded53bd68c85f197d1fa758e6bc7bc1
alez1986@gmail.com <sturman7>
2021-02-25 00:47:49 +0100

    Added cmake and main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Local
+#include "ResourceManager.h"
+
+// STL
+#include <iostream>
+#include <sstream>
+
+
+using namespace std;
+
+int main()
+{
+  ResourceManager rm(100);
+  cout << "Resource Manager" << endl;
+  return 0;
+}
